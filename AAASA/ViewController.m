789cb0c50076eb619a9afa788d89abc0bb2060da
zhangjian <zhangjian@yongche.com>
2016-01-27 13:00:51 +0800

    1 commit

--- AAASA/ViewController.m
+++ AAASA/ViewController.m
@@ -9,14 +9,21 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) int sum;  
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Dispose of any resources that can be recreated.
+    
+    int a = 5;
+    int b = 10;
+    
+    self.sum = a + b;
+    
+    NSLog(@"The result is: %d", self.sum);
 }
 
 - (void)didReceiveMemoryWarning {
