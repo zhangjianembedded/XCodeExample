b5a7561d2bc72a1024adb32b8ecbd7b0b80805c4
zhangjian <zhangjian@yongche.com>
2016-01-27 12:52:36 +0800

    Initial Commit

---
+++ AAASA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAASA
+//
+//  Created by yongche on 16/1/27.
+//  Copyright © 2016年 study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AAASA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AAASA
+//
+//  Created by yongche on 16/1/27.
+//  Copyright © 2016年 study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
